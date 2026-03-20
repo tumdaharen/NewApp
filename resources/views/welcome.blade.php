c31bd95fea3b184a6b41f8c5aa4db685e66db085
Harendra Tumda <tumdaharen78@gmail.com>
2026-03-20 17:47:12 +0530

    added login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html class="h-full" clang="en">
 
 <head>
     <meta charset="UTF-8">
@@ -7,12 +7,43 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Haren Application</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @fluxAppearance
 </head>
 
-<body>
-    <div>
-        <h1 class=" text-red-500">Hello World!</h1>
+<body class="h-full">
+    <div class="flex justify-center items-center h-full">
+        <flux:card class="space-y-6">
+            <div>
+                <flux:heading size="lg">Log in to your account</flux:heading>
+                <flux:text class="mt-2">Welcome back!</flux:text>
+            </div>
+
+            <div class="space-y-6">
+                <flux:input label="Email" type="email" placeholder="Your email address" />
+
+                <flux:field>
+                    <div class="mb-3 flex justify-between">
+                        <flux:label>Password</flux:label>
+
+                        <flux:link href="#" variant="subtle" class="text-sm">Forgot password?</flux:link>
+                    </div>
+
+                    <flux:input type="password" placeholder="Your password" />
+
+                    <flux:error name="password" />
+                </flux:field>
+            </div>
+
+            <div class="space-y-2">
+                <flux:button variant="primary" class="w-full">Log in</flux:button>
+
+                <flux:button variant="ghost" class="w-full">Sign up for a new account</flux:button>
+            </div>
+        </flux:card>
     </div>
+
+    @livewireScripts
+    @fluxScripts
 </body>
 
 </html>
